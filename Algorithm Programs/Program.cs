b62834df7b875158a86511f57b58af163784fde8
Anant1202 <anantsingh1202@gmail.com>
2022-05-31 18:46:32 +0530

    Welcome to Algorithm Programs

---
+++ Algorithm Programs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AlgorithmPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+        }
+    }
+}
+
