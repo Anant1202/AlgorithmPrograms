60235ec85dfee9f20be8466e00b7ca3d22e5c8b4
Anant1202 <anantsingh1202@gmail.com>
2022-05-31 20:02:46 +0530

    1.Permutation Program

---
+++ Algorithm Programs/Permutation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs
+{
+    public class Permutation
+    {
+        public static void Permute(string s, int start, int end)
+        {
+            if (start == end)
+            {
+                Console.WriteLine("\ns");
+            }
+            else
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    s = (Swap(s, start, i));
+                    //Recursion 
+                    //start + 1 is used here to fix the first character
+                    Permute(s, start + 1, end);
+                    //Backtrack
+                    s = Swap(s, start, i);
+                }
+            }
+
+        }
+        public static string Swap(string word, int start, int end)
+        {
+            char temp;
+            char[] array = word.ToCharArray();
+            temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            string s = new string(array);
+            return s;
+
+        }
+    }
+}
--- Algorithm Programs/Program.cs
+++ Algorithm Programs/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace AlgorithmPrograms
+namespace Algorithm_Programs
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs");
+            Console.WriteLine("Enter String to find Permutation ");
+            string obj = Console.ReadLine();
+            int n = obj.Length;
+            Permutation.Permute(obj, 0, n - 1);
         }
     }
 }
