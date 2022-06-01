2d2e4ec1696a438a9b59576dd4651157e12943fb
Anant1202 <anantsingh1202@gmail.com>
2022-06-01 17:03:16 +0530

    3.Insertion Sort Program

---
+++ Algorithm Programs/InsertionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs
+{
+    public class InsertionSort
+    {
+        public void InsertionSorting(string[] array)
+        {
+            int i, j;
+            //Initially i is kept at 1 and j is kept at 0 to compare 
+            for (i = 1; i < array.Length; i++)
+            {
+                string value = array[i];
+                j = i - 1;
+                while ((j >= 0) && (array[j].CompareTo(value) > 0))
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = value;
+            }
+        }
+    }
+}
--- Algorithm Programs/Program.cs
+++ Algorithm Programs/Program.cs
@@ -6,11 +6,18 @@ namespace Algorithm_Programs
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm Programs");
-            Console.WriteLine("Enter String to find Permutation ");
-            string obj = Console.ReadLine();
-            int n = obj.Length;
-            Permutation.Permute(obj, 0, n - 1);
+            //Console.WriteLine("Welcome to Algorithm Programs");
+            //Console.WriteLine("Enter String to find Permutation ");
+            //string obj = Console.ReadLine();
+            //int n = obj.Length;
+            //Permutation.Permute(obj, 0, n - 1);
+            InsertionSort obj = new InsertionSort();
+            string[] names = { "Sameer", "Raj", "Aryan", "Rahul" };
+            obj.InsertionSorting(names);
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
